c90bdaa817e43c11af16b0014f3fec9962ca0bd6
threezinedine <threezinedine@gmail.com>
2026-02-07 22:34:12 +0700

    [feature] add more tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -40,38 +40,36 @@ def test_parse_repeat():
     assert parser.validate("b").size == 0
 
 
-# def test_parse_complex():
-#     pattern = "a|b*"
-#     parser = Parser.parse(pattern)
+def test_parse_complex():
+    parser = Parser("a|b*")
 
-#     assert parser.validate("a").valid is True
-#     assert parser.validate("a").size == 1
+    assert parser.validate("a").valid is True
+    assert parser.validate("a").size == 1
 
-#     assert parser.validate("").valid is True
-#     assert parser.validate("").size == 0
+    assert parser.validate("").valid is True
+    assert parser.validate("").size == 0
 
-#     assert parser.validate("b").valid is True
-#     assert parser.validate("b").size == 1
+    assert parser.validate("b").valid is True
+    assert parser.validate("b").size == 1
 
-#     assert parser.validate("bb").valid is True
-#     assert parser.validate("bb").size == 2
+    assert parser.validate("bb").valid is True
+    assert parser.validate("bb").size == 2
 
-#     assert parser.validate("c").valid is True
-#     assert parser.validate("c").size == 0
+    assert parser.validate("c").valid is True
+    assert parser.validate("c").size == 0
 
 
-# def test_parse_concatenated_repeat():
-#     pattern = "ab*"
-#     parser = Parser.parse(pattern)
+def test_parse_concatenated_repeat():
+    parser = Parser("ab*")
 
-#     assert parser.validate("a").valid is True
-#     assert parser.validate("a").size == 1
+    assert parser.validate("a").valid is True
+    assert parser.validate("a").size == 1
 
-#     assert parser.validate("ab").valid is True
-#     assert parser.validate("ab").size == 2
+    assert parser.validate("ab").valid is True
+    assert parser.validate("ab").size == 2
 
-#     assert parser.validate("abb").valid is True
-#     assert parser.validate("abb").size == 3
+    assert parser.validate("abb").valid is True
+    assert parser.validate("abb").size == 3
 
-#     assert parser.validate("b").valid is False
-#     assert parser.validate("b").size == -1
+    assert parser.validate("b").valid is False
+    assert parser.validate("b").size == -1
